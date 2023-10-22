35d9d1b88f234bebdc8e0b92339d8394219a74ba
zalup4k228 <147541094+zalup4k228@users.noreply.github.com>
2023-10-22 04:13:28 +0300

    Update scr.js

--- scr.js
+++ scr.js
@@ -4,15 +4,15 @@ tg.expand();
 
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#FF00FF";
-
 let btn = document.getElementById("btn");
-var pass = document.getElementById("pass");
-var ang = document.getElementById("ang");
-var data = pass + ang;
+var pass = document.getElementById("pass").value; // Получаем значение поля пароля
+    var ang = document.getElementById("ang").value; // Получаем значение поля номера договора
+    var data = pass + ' ' + ang;
 
 
 btn.addEventListener("click", function(){
     tg.MainButton.setText("Сообщение отправлено!");
     tg.MainButton.show();
-    tg.sendData(data);
+    tg.sendData(pass);
+    tg.sendData(ang);
 });
