1c543a10f84ac196fae45a446ecdad8aadf8ac42
zalup4k228 <147541094+zalup4k228@users.noreply.github.com>
2023-10-22 04:27:40 +0300

    Update scr.js

--- scr.js
+++ scr.js
@@ -2,13 +2,10 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#FF00FF";
-let btn = document.getElementById("btn");
-var pass = document.getElementById("pass").value; // Получаем значение поля пароля
-    var ang = document.getElementById("ang").value; // Получаем значение поля номера договора
-
 btn.addEventListener("click", function(){
+    var pass = document.getElementById("pass").value; // Получаем актуальное значение поля пароля
+    var ang = document.getElementById("ang").value; // Получаем актуальное значение поля номера договора
+    
     tg.MainButton.setText("Сообщение отправлено!");
     tg.MainButton.show();
     tg.sendData(pass);
