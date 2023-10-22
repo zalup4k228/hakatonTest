aa4389bb228e4dcf5b3b972eedd217f7d755265c
zalup4k228 <147541094+zalup4k228@users.noreply.github.com>
2023-10-22 04:00:53 +0300

    Update scr.js

--- scr.js
+++ scr.js
@@ -8,11 +8,11 @@ tg.MainButton.color = "#FF00FF";
 let btn = document.getElementById("btn");
 var pass = document.getElementById("pass").value;
 var ang = document.getElementById("ang").value;
+var data = pass + ang;
 
 
 btn.addEventListener("click", function(){
     tg.MainButton.setText("Сообщение отправлено!");
     tg.MainButton.show();
-    tg.sendData(pass.nextLine());
-    tg.sendData(ang.nextLine());
+    tg.sendData(data);
 });
